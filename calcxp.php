b410a2c870fd0cbe511a9b4136320a6adcf2c3b4
Vitor Matos <vitormatosqs@gmail.com>
2026-04-05 00:42:19 -0300

    caculadora de xp

--- calcxp.php
+++ calcxp.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de XP</title>
-</head>
-<body>
-    <form method="POST" action="calxp.php">
-        <label>Nível Atual</label>
-        <input type="text" name="nivel">
-
-        <label>Xp Acumulado</label>
-        <input type="text" name="acumu">
-
-        <label>Dificuldade de Missão</label>
-        <input type="text" name="dificul" placeholder="Ex: Fácil, Média ou Difícil">
-
-        <button type="submit">Calcular</button>
-    </form>
-
-    <?php
+<?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $difi = $_POST['dificul'];
-        $acumu = $_POST['acumu'];
+        $acumu = $_POST['xp'];
         $nvatual = $_POST['nivel'];
         $vbase = 100;
 
-        $xptotal += $vbase;
+        $xptotal = $acumu + $vbase;
 
         if(($difi) == "media") {
             $xp = $xptotal * 1.5;
@@ -35,14 +14,44 @@
             $xp = $xptotal * 2;
             $xptotal += $xp;
         }
-
-        if(floatval($xptotal) >= 1000) {
-            $nivelnovo = "Parabéns você subiu para o nível ". (floatval($nvatual) + 1);
-        }
-
-        header("Location: calcxp.php?xp=" . number_format($xptotal, 2)) . "&nivel=" . $nvatual;
     }
 
     ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="calcxp.css">
+    <title>Calculadora de XP</title>
+</head>
+<body>
+    <form method="POST" action="calcxp.php">
+        <label>Nível Atual</label>
+        <input type="text" name="nivel">
+
+        <label>Xp Acumulado</label>
+        <input type="text" name="xp">
+
+        <label>Dificuldade de Missão</label>
+        <input type="text" name="dificul" placeholder="Ex: Fácil, Média ou Difícil">
+
+        <button type="submit">Calcular</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (floatval($xptotal) >= 1000) {
+        $nvnovo = (floatval($nvatual) + 1);
+        echo "<div><h4>Parabéns você subiu para o nível " . $nvnovo . "</h4>";
+        echo "<h4>XP Total:" . $xptotal . "</h4><div>";
+    } else {
+        echo "<div><h4>XP Total:" . $xptotal . "</h4>";
+        echo "<h4>Nivel:" . $nvatual . "</h4></div>";
+    }
+    }
+    ?>
+
 </body>
 </html>
